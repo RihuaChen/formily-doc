13b76a418493b641f679ee33e630384b8cd5594c
ricky chen <rickychenrihua@foxmail.com>
2020-11-10 18:03:13 +0800

    test build 4

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 import path from 'path';
 
 export default defineConfig({
+  base: '/formily-doc/',
+  publicPath: '/formily-doc/',
   title: '表单解决方案',
   mode: 'site',
   // more config: https://d.umijs.org/config
